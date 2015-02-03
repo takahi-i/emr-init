5c584af98e170ff5b4214b8de4cbfbb2cafc8dd1
takahi-i <takahiko03@gmail.com>
2015-02-03 11:07:36 +0900

    Tiny fix

--- init.sh
+++ init.sh
@@ -1,14 +1,21 @@
 # SETTING
 MAVEN_VERSION=3.2.5
+MAVEN_PACKAGE=apache-maven-$MAVEN_VERSION-bin
 
 # install packages
 sudo yum install -y wget
 sudo yum install -y git
 
 # install maven
-wget http://ftp.yz.yamagata-u.ac.jp/pub/network/apache/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
-tar xvf apache-maven-$MAVEN_VERSION-bin.tar.gz
-sudo mv apache-maven-$MAVEN_VERSION /opt/
+wget http://ftp.yz.yamagata-u.ac.jp/pub/network/apache/maven/maven-3/$MAVEN_VERSION/binaries/$MAVEN_PACKAGE.tar.gz
+tar xvf $MAVEN_PACKAGE.tar.gz
+
+if [ -e /opt/$MAVEN_PACKAGE ]; then
+    echo "maven is installed"
+else
+    echo "maven is NOT installed"
+    sudo mv $MAVEN_PACKAGE /opt/
+fi
 
 # setting env variables
 cat cat <<EOT
