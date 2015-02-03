07afb36176d63515df2e20e8c512c45ced0a64c5
takahi-i <takahiko03@gmail.com>
2015-02-03 12:05:30 +0900

    Fix a bug

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@ MAVEN_PACKAGE=apache-maven-$MAVEN_VERSION
 # install packages
 sudo yum install -y wget
 sudo yum install -y git
+sudo yum install -y emacs
+sudo yum install -y ant
 
 # install maven
 wget http://ftp.yz.yamagata-u.ac.jp/pub/network/apache/maven/maven-3/$MAVEN_VERSION/binaries/$MAVEN_PACKAGE-bin.tar.gz
