910697d46e853a24b87b15f73691fbd45f534858
takahi-i <takahiko03@gmail.com>
2015-02-03 11:02:12 +0900

    Tiny fix

--- init.sh
+++ init.sh
@@ -7,9 +7,12 @@ sudo yum install -y git
 
 # install maven
 wget http://ftp.yz.yamagata-u.ac.jp/pub/network/apache/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
-sudo mv apache-maven-3.2.5 /opt/
+tar xvf apache-maven-$MAVEN_VERSION-bin.tar.gz
+sudo mv apache-maven-$MAVEN_VERSION /opt/
 
 # setting env variables
-export M3_HOME=/opt/apache-maven-$MAVEN_VERSION
-M3=$M3_HOME/bin
-export PATH=$M3:$PATH
+cat cat <<EOT
+export M3_HOME=/opt/apache-maven-$MAVEN_VERSION >> $HOME/.bashrc
+M3=$M3_HOME/bin >> $HOME/.bashrc
+export PATH=$M3:$PATH >> $HOME/.bashrc
+EOT
